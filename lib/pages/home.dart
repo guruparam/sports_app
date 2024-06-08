889d5c23954a5ca793b197b57a83a317a058023e
Paramaguru <paramagurusrm@gmail.com>
2024-06-08 20:20:57 +0530

    Login and sigup page setup.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
 
   void logout() {
     print("logout");
-    Navigator.pushNamed(context, '/');
+    Navigator.pushNamed(context, '/auth');
   }
 
   @override
